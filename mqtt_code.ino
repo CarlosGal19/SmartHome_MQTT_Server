0a3298350bb948c4a03c8b57c4c9a742628b27ef
Carlos Galindo <utm22030587@utma.edu.mx>
2025-12-07 14:53:00 -0600

    feat: add presence sensor code

--- mqtt_code.ino
+++ mqtt_code.ino
@@ -1,18 +1,20 @@
+#include <driver/ledc.h>
 #include <WiFi.h>
 #include <PubSubClient.h>
 
-//const char* ssid = "IZZI-A8A4";
-const char* ssid = "DESKTOP-KALDOS7 3748";
-//const char* password = "nPTyfhpEmALRLF9tcc";
-const char* password = "9=4p72L3";
+const char* ssid = "IZZI-A8A4";
+// const char* ssid = "DESKTOP-KALDOS7 3748";
+const char* password = "nPTyfhpEmALRLF9tcc";
+// const char* password = "9=4p72L3";
 
-const char* mqtt_server = "89.116.191.188";
+// const char* mqtt_server = "89.116.191.188";
+const char* mqtt_server = "3.83.35.123";
 
 WiFiClient espClient;
 PubSubClient mqttClient(espClient);
 
 // ===================================================
-// VARIABLES DE ESTADO
+// STATE VARIABLES
 // ===================================================
 bool garage_status = false;
 bool living_room_status = false;
@@ -23,12 +25,15 @@ bool bathroom_status = false;
 bool kitchen_status = false;
 bool patio_status = false;
 
-bool door_status = false;
-bool window_status = false;
-bool blind_status = false;
+bool door_1_status = false;
+bool door_2_status = false;
+bool window_1_status = false;
+bool window_2_status = false;
+
+bool buzzer_status = false;
 
 // ===================================================
-// ESTRUCTURAS DE SALIDAS
+// OUTPUT/INPUT STRUCTURES
 // ===================================================
 struct outputItem {
   const char* name;
@@ -36,22 +41,40 @@ struct outputItem {
   bool* stateVar;
 };
 
+struct inputButton {
+  const char* name;
+  int pin;
+  bool* stateVar;
+};
+
 outputItem leds[] = {
-  {"garage", 5, &garage_status},
-  {"living_room", 18, &living_room_status},
-  {"room_1", 19, &room_1_status},
-  {"room_2", 21, &room_2_status},
-  {"hallway", 22, &hallway_status},
-  {"bathroom", 23, &bathroom_status},
-  {"kitchen", 25, &kitchen_status},
-  {"patio", 26, &patio_status}
+  { "living_room", 18, &living_room_status },
+  { "room_1", 19, &room_1_status },
+  { "room_2", 21, &room_2_status },
+  { "bathroom", 23, &bathroom_status },
+  { "patio", 26, &patio_status }
+};
+
+inputButton ledButtons[] = {
+  { "living_room", 13, &living_room_status },
+  { "room_1", 14, &room_1_status },
+  { "room_2", 32, &room_2_status },
+  { "bathroom", 33, &bathroom_status },
+  { "patio", 15, &patio_status }
 };
 
 outputItem motors[] = {
-  {"door", 27, &door_status},
-  {"window", 32, &window_status},
-  {"blind", 33, &blind_status}
+  { "door_1", 4, &door_1_status },
+  { "door_2", 5, &door_2_status },
+  { "window_1", 12, &window_1_status },
+  { "window_2", 16, &window_2_status },
 };
+
+outputItem buzzer = { "buzzer", 25, &buzzer_status };
+
+int ledButtonsCount = sizeof(ledButtons) / sizeof(ledButtons[0]);
+int ledsCount = sizeof(leds) / sizeof(leds[0]);
+int motorsCount = sizeof(motors) / sizeof(motors[0]);
 
 // ===================================================
 // WIFI
@@ -71,7 +94,7 @@ void setup_wifi() {
 }
 
 // ===================================================
-// MQTT CALLBACK — SOLO LEE smarthome/.../set
+// MQTT CALLBACK — READ smarthome/${NAME}/set
 // ===================================================
 void mqtt_callback(char* topic, byte* payload, unsigned int length) {
   String message;
@@ -86,7 +109,6 @@ void mqtt_callback(char* topic, byte* payload, unsigned int length) {
   Serial.print(" => ");
   Serial.println(message);
 
-  int ledsCount = sizeof(leds) / sizeof(leds[0]);
   for (int i = 0; i < ledsCount; i++) {
     String expected = String("smarthome/") + leds[i].name + "/set";
 
@@ -104,7 +126,6 @@ void mqtt_callback(char* topic, byte* payload, unsigned int length) {
     }
   }
 
-  int motorsCount = sizeof(motors) / sizeof(motors[0]);
   for (int i = 0; i < motorsCount; i++) {
     String expected = String("smarthome/") + motors[i].name + "/set";
 
@@ -112,7 +133,6 @@ void mqtt_callback(char* topic, byte* payload, unsigned int length) {
       bool newState = (message == "1");
 
       *(motors[i].stateVar) = newState;
-      digitalWrite(motors[i].pin, newState ? HIGH : LOW);
 
       String statusTopic = String("smarthome/") + motors[i].name + "/status";
       mqttClient.publish(statusTopic.c_str(), newState ? "1" : "0");
@@ -120,6 +140,18 @@ void mqtt_callback(char* topic, byte* payload, unsigned int length) {
       Serial.printf("MOTOR %s SET => %d\n", motors[i].name, newState);
       return;
     }
+  }
+  
+  if (String(topic) == "smarthome/buzzer/set") {
+    bool newState = false;
+
+    *(buzzer.stateVar) = newState;
+
+    String statusTopic = "smarthome/buzzer/status";
+    mqttClient.publish(statusTopic.c_str(), "0");
+    noTone(buzzer.pin);
+    Serial.printf("BUZZER %s SET => %d\n", buzzer.name, newState);
+    return;
   }
 }
 
@@ -139,7 +171,6 @@ void setup_mqtt() {
     if (mqttClient.connect(clientId.c_str())) {
       Serial.println("Connected!");
 
-      int ledsCount = sizeof(leds) / sizeof(leds[0]);
       for (int i = 0; i < ledsCount; i++) {
         String topic = String("smarthome/") + leds[i].name + "/set";
         mqttClient.subscribe(topic.c_str());
@@ -147,13 +178,17 @@ void setup_mqtt() {
         Serial.println(topic);
       }
 
-      int motorsCount = sizeof(motors) / sizeof(motors[0]);
       for (int i = 0; i < motorsCount; i++) {
         String topic = String("smarthome/") + motors[i].name + "/set";
         mqttClient.subscribe(topic.c_str());
         Serial.print("Subscribed: ");
         Serial.println(topic);
       }
+
+      String topic = "smarthome/buzzer/set";
+      mqttClient.subscribe(topic.c_str());
+      Serial.print("Subscribed: ");
+      Serial.println(topic);
 
     } else {
       Serial.print("FAILED. rc=");
@@ -170,18 +205,20 @@ void setup_mqtt() {
 void setup() {
   Serial.begin(115200);
 
-  int ledsCount = sizeof(leds) / sizeof(leds[0]);
-  int motorsCount = sizeof(motors) / sizeof(motors[0]);
-
   for (int i = 0; i < ledsCount; i++) {
     pinMode(leds[i].pin, OUTPUT);
     digitalWrite(leds[i].pin, LOW);
   }
 
   for (int i = 0; i < motorsCount; i++) {
-    pinMode(motors[i].pin, OUTPUT);
-    digitalWrite(motors[i].pin, LOW);
+    pinMode(motors[i].pin, INPUT);
   }
+
+  for (int i = 0; i < ledButtonsCount; i++) {
+    pinMode(ledButtons[i].pin, INPUT);
+  }
+
+  pinMode(buzzer.pin, OUTPUT);
 
   setup_wifi();
   setup_mqtt();
@@ -190,24 +227,58 @@ void setup() {
 // ===================================================
 // LOOP
 // ===================================================
+
+unsigned long lastPressButtons[5] = { 0 };
+unsigned long lastPressMotors[4] = { 0 };
+const unsigned long debounceDelay = 500;
+
 void loop() {
   if (!mqttClient.connected()) {
     setup_mqtt();
   }
   mqttClient.loop();
 
-  static unsigned long lastToggle = 0;
-  if (millis() - lastToggle >= 5000) {
+  for (int i = 0; i < ledButtonsCount; i++) {
+    if (digitalRead(ledButtons[i].pin) == HIGH) {
 
-    patio_status = !patio_status;
-    digitalWrite(26, patio_status ? HIGH : LOW);
+      if (millis() - lastPressButtons[i] > debounceDelay) {
 
-    mqttClient.publish("smarthome/patio/status", patio_status ? "1" : "0");
+        *ledButtons[i].stateVar = !(*ledButtons[i].stateVar);
 
-    Serial.print("Toggled patio => ");
-    Serial.println(patio_status ? "1" : "0");
+        digitalWrite(leds[i].pin, (*ledButtons[i].stateVar) ? HIGH : LOW);
 
-    lastToggle = millis();
+        String topic = String("smarthome/") + ledButtons[i].name + "/status";
+
+        String payload = (*ledButtons[i].stateVar) ? "1" : "0";
+
+        Serial.println(topic);
+        Serial.println(digitalRead(ledButtons[i].pin));
+        Serial.println(leds[i].pin);
+
+        mqttClient.publish(topic.c_str(), payload.c_str());
+        lastPressButtons[i] = millis();
+      }
+    }
+  }
+
+  for (int i = 0; i < motorsCount; i++) {
+    Serial.println(digitalRead(motors[3].pin));
+    if ((digitalRead(motors[i].pin) == HIGH) && (*motors[i].stateVar) && (!(*buzzer.stateVar))) {
+
+      if (millis() - lastPressMotors[i] > debounceDelay) {
+        *buzzer.stateVar = true;
+        tone(buzzer.pin, 440);
+
+        String topic = String("smarthome/buzzer/status");
+
+        String payload = "1";
+
+        Serial.println(topic);
+        Serial.println(digitalRead(buzzer.pin));
+
+        mqttClient.publish(topic.c_str(), payload.c_str());
+        lastPressButtons[i] = millis();
+      }
+    }
   }
 }
-
